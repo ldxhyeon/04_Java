0e037b2927afa1f83cd7969635bea232f84be216
ldxhyeon <dlehdgus3180@naver.com>
2024-07-19 00:12:08 +0900

    2024_07_19 예제문제

---
+++ 04_array/src/run/main.java
@@ -0,0 +1,178 @@
+package run;
+
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// 1번문제
+//		int sum = 0;
+//		
+//		int[] arr = new int[9];
+//		
+//		for(int i = 0; i < arr.length; i++) {
+//			arr[i] = i+1;
+//			System.out.print(arr[i] + " ");
+//			
+//			if(i % 2 == 0) {
+//			
+//				sum += arr[i];
+//				
+//			}
+//		}
+//		
+//		System.out.println("\n짝수 번째 인덱스 합 : "  + sum);
+		 
+		
+		
+		// 2번문제
+//		int sum = 0;
+//		
+//		int[] arr = new int[9];
+//		
+//		for(int i = 0; i < arr.length; i++) {
+//			// 0 1 2 3 4 5 6 7 8
+//			
+//			arr[i] = arr.length - i;
+//				
+//			System.out.print(arr[i] + " ");
+//			
+//
+//			if(i % 2 != 0) {
+//			
+//				sum += arr[i];
+//		
+//			}
+//		}
+//		
+//		System.out.println("\n홀수 번째 인덱스 합 : "  + sum);
+		
+		
+		// 3번 문제
+//		System.out.println("배열 크기를 입력하세요.");
+//		int a = sc.nextInt();
+//		
+//		int[] arr= new int[a];
+//		
+//		for(int i =0; i < arr.length; i++) {
+//			System.out.print(i+1 + " ");
+//		}
+		
+		
+//		boolean found = false;
+//		
+//		int[] arr = new int[5];
+//		
+// 		for(int i = 0; i < arr.length; i++) {
+// 			System.out.printf("입력 %d :", i);
+// 			arr[i] = sc.nextInt();
+// 		}
+// 		
+// 		System.out.println("검색할 값 :");
+// 		int search = sc.nextInt();
+// 		
+// 		for(int i = 0; i < arr.length; i++) {
+// 			if(arr[i] == search) {
+// 				System.out.println("인덱스 : " + i);
+// 				found = true;
+// 			}
+// 		}
+// 		
+// 		if(!found) {
+//				System.out.println("일치하는 값이 존재하지 않습니다.");
+//			}
+// 		
+ 		
+//		int sum = 0;
+//		
+//		System.out.println("배열길이를 입력하세요");
+//		int a = sc.nextInt();
+//		
+//		int[] arr = new int[a];
+//		
+//		for(int i = 0; i < arr.length; i++) {
+//			System.out.printf("배열 %d번쨰 인덱스에 넣을 값 : ",i);
+//			arr[i] = sc.nextInt();
+//		}
+//		
+//		for(int i = 0; i < arr.length; i++) {
+//			System.out.print(arr[i]+ " ");
+//			sum += arr[i];
+//		}
+//		
+//		System.out.println("\n총합 : " + sum);
+		
+		
+		
+		
+		
+		
+		
+//		char[] arr = new char[14];
+//		
+//		System.out.print("주민등록번호(-포함) : ");
+//		String str = sc.next();
+//		
+//		// 주민등록번호를 char 배열 요소로 하나씩 대입
+//		for(int i = 0 ; i < str.length() ; i++) {
+//			if(i <= 7) 	arr[i] = str.charAt(i); // 성별 까지
+//			else		arr[i] = '*';			// 성별 이후
+//			
+//			System.out.print(arr[i]); // 출력
+//		}
+		
+		
+//		char[] arr = new char[14];
+//		System.out.println("주민등록번호(-포함)");
+//		String str = sc.next();
+//		
+//		for(int i = 0; i < str.length(); i++) {
+//			if( i <= 7) {
+//				arr[i] = str.charAt(i);
+//			}else {
+//				arr[i] = '*';
+//			}
+//			
+//			System.out.print(arr[i]);
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	 
+		
+		
+		
+		
+	}
+}
