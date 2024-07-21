112f243075ca6c093bb58fff70a7458445c6d1d4
ldxhyeon <dlehdgus3180@naver.com>
2024-07-22 00:37:25 +0900

    2024_07_22 Java 예제문제

--- 04_array/src/run/main.java
+++ 04_array/src/run/main.java
@@ -140,8 +140,134 @@ public class main {
 		
 		
 		
+//		int n;
+//		
+//		
+//		while(true) {
+//			
+//			System.out.print("정수 : ");
+//			n = sc.nextInt();
+//			
+//			if(n >= 3 && n % 2 != 0) {
+//				int[] arr = new int[n];
+//				int mid = n / 2;
+//				
+//				// i + 1= 0+1, 1+1 2+1 이니까 3
+//				// arr[0] == 1
+//				// arr[1] == 2
+//				// arr[2] == 3
+//				
+//				for (int i = 0; i <= mid; i++) {
+//                    arr[i] = i + 1;
+//                }
+//				
+//				
+//				// 2+1 == i
+//				// 3 < 5이므로 3,4 
+//				// arr[3] == 5-3 이므로 2 
+//				// arr[4] == 4-3 이므로 1
+//                for (int i = mid + 1; i < n; i++) {
+//                    arr[i] = n - i;
+//                }
+//
+//                // 배열 출력
+//                for (int i = 0; i < n; i++) {
+//                    System.out.print(arr[i] + " ");
+//                }
+//                System.out.println(); // 줄 바꿈 추가
+//
+//                break; // 유효한 입력을 받았으므로 루프 종료
+//				
+//				
+//			}else {
+//				System.out.println("다시 입력하세요.");
+//			}
+//			
+//		}
 		
 		
+		
+//		int[] arr = new int[10];
+//		
+//		System.out.print("발생한 난수  : ");
+//		for(int i = 0; i < arr.length; i++) {
+//			
+//			arr[i] = (int)Math.floor(Math.random() * 10 + 1);
+//			
+//			System.out.print(arr[i] + " ");
+//			
+//		}
+		
+		
+		int[] arr = new int[10];
+		
+		
+		System.out.print("발생한 난수  : ");
+		
+		
+		for(int i = 0; i < arr.length; i++) {
+		
+			arr[i] = (int)Math.floor(Math.random() * 10 + 1);
+			// 1부터 10 난수
+			
+			System.out.print(arr[i] + " ");
+		}
+		
+		
+		int max = arr[0];
+		
+		
+		int min = arr[0];
+		
+		
+		
+		for (int i = 1; i < arr.length; i++) {
+			
+            if (arr[i] > max) {
+            	
+                max = arr[i];
+            }
+				
+            
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+		
+		// 결과 출력
+        System.out.println("\n최대값: " + max);
+        System.out.println("최소값: " + min);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
 		
 		
 		
