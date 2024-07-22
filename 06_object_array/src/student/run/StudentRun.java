976c14d44b64c68995910624cd5a9701c4529cad
ldxhyeon <dlehdgus3180@naver.com>
2024-07-22 17:48:30 +0900

    2024_07_22 학생명단관리

---
+++ 06_object_array/src/student/run/StudentRun.java
@@ -0,0 +1,13 @@
+package student.run;
+
+import student.view.StudentView;
+
+public class StudentRun {
+	public static void main(String[] args) {
+		
+		
+		// StudentView 객체 생성
+		StudentView view = new StudentView();
+		view.mainMenu();
+	}
+}
