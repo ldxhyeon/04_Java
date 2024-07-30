9c4e219986efa50f88f44bb1181f95b85e3d991c
ldxhyeon <dlehdgus3180@naver.com>
2024-07-30 17:32:23 +0900

    2024_07_30 예외처리

---
+++ 10_exception/src/pkg/run/TryCatchFinallyRun.java
@@ -0,0 +1,12 @@
+package pkg.run;
+
+import pkg.service.TryCatchFinallyService;
+
+public class TryCatchFinallyRun {
+	public static void main(String[] args) {
+		TryCatchFinallyService service = new TryCatchFinallyService();
+		
+//		service.test1();
+		service.test2();
+	}
+}
