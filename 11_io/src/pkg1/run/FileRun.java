b231aa4f767ee790b3bde47da15dfa55c7574d56
ldxhyeon <dlehdgus3180@naver.com>
2024-07-31 17:47:56 +0900

    2024_07_31 예외처리,입출력

---
+++ 11_io/src/pkg1/run/FileRun.java
@@ -0,0 +1,18 @@
+package pkg1.run;
+
+import java.io.File;
+
+import pkg1.service.FileService;
+
+public class FileRun {
+
+	public static void main(String[] args) {
+		
+		FileService service = new FileService();
+		
+//		service.test1();
+//		service.test2();
+		service.test3();
+	}
+	
+}
